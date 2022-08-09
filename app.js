a4db14d791299ed6c799695ffd4f8284afb5f448
Boglarka Bodnar <bodnarbog94@gmail.com>
2022-08-09 22:14:00 +0200

    up

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-const rock = document.querySelector(".rock")
-const paper = document.querySelector(".paper")
-const scissors = document.querySelector(".scissors")
-const gameText = document.querySelector(".js-game-text")
 const options = ["rock", "paper", "scissors"]
-
 let player
 let computer
 let result
 
-rock.addEventListener("click", function () { player = "rock", start() })
-paper.addEventListener("click", function () { player = "paper", start() })
-scissors.addEventListener("click", function () { player = "scissors", start() })
+document.querySelector(".rock").addEventListener("click", function () { player = "rock", start() })
+document.querySelector(".paper").addEventListener("click", function () { player = "paper", start() })
+document.querySelector(".scissors").addEventListener("click", function () { player = "scissors", start() })
 
 function start() {
     computer = options[Math.floor(Math.random() * options.length)]
@@ -29,5 +24,5 @@ function start() {
     else {
         result = "You won!"
     }
-    gameText.innerHTML = `<p> Player's choice: ${player}. The choice of the computer: ${computer}. ${result}</p>`
+    document.querySelector(".js-game-text").innerHTML = `<p> Player's choice: ${player}. The choice of the computer: ${computer}. ${result}</p>`
 }
